c439068ec83eceb2fffdde38a52978ba9aa2246d
ApostalDante <apostal666666@gmail.com>
2022-09-05 20:47:01 +0300

    create dz8

---
+++ Program.cs
@@ -0,0 +1,317 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+/* - решение - */
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArrayRandom(rows, columns, 0, 10);
+
+PrintArray(array);
+SortArrayLines(array);
+Console.WriteLine($"\nОтсортированный массив: ");
+PrintArray(array);
+
+
+int[,] GetArrayRandom(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void SortArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+/* - решение - */
+
+int m = 0;
+int n = 0;
+
+Console.WriteLine($"\nВведите размер массива m x n, количество строк массива - должно быть равным количеству столбцов массива!");
+do
+{
+    Console.Write("Введите количество строк массива: ");
+    m = int.Parse(Console.ReadLine() ?? "");
+    Console.Write("Введите количество столбцов массива: ");
+    n = int.Parse(Console.ReadLine() ?? "");
+} while (m != n);
+
+
+int[,] array2 = GetArrayRandom(m, n, 0, 10);
+int minSumLine = 0;
+
+PrintArray(array2);
+int sumLine = SumLineElements(array2, 0);
+SearchDownSumLineElements(array2, sumLine);
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой элементов ");
+
+
+void SearchDownSumLineElements(int[,] array, int sumLine)
+{
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int tempSumLine = SumLineElements(array, i);
+        if (sumLine > tempSumLine)
+        {
+            sumLine = tempSumLine;
+            minSumLine = i;
+        }
+    }
+}
+
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+/*Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+/* - решение - */
+
+Console.WriteLine($"\nРазмер матриц и диапазон случайных значений берутся из предыдущего запроса");
+
+int[,] firstMartrix = GetArrayRandom(m, n, 0, 10);
+Console.WriteLine($"\nПервая матрица:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = GetArrayRandom(m, n, 0, 10);
+Console.WriteLine($"\nВторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m, n];
+
+
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+PrintArray(resultMatrix);
+
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+/*Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+/* - решение - */
+
+Console.WriteLine($"\nВведите размер массива X x Y x Z:");
+Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите Z: ");
+int z = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine();
+
+int[,,] array3D = new int[x, y, z];
+CreateArray(array3D);
+PrintArray3D(array3D);
+
+
+void PrintArray3D(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"{array3D[i, j, k]}({i},{j},{k}) ");
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+void CreateArray(int[,,] array3D)
+{
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+/*Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+/* - решение - */
+
+Console.WriteLine($"\nЗаполнить спирально массив 4 на 4.");
+
+int q = 4;
+int[,] sqareMatrix = new int[q, q];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+    sqareMatrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+PrintArraySqare(sqareMatrix);
+
+void PrintArraySqare(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($"0{array[i, j]} ");
+
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+/*--------------------------------------------------------------------------------------*/
+
